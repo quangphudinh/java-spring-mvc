3252b16b958a41dcc9f157800efc2bc4bc577f9f
John Doe <johndoe@email.com>
2025-02-05 14:23:26 +0700

    video17

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,51 +1,51 @@
-package vn.hoidanit.laptopshop.domain;
+// package vn.hoidanit.laptopshop.domain;
 
-import java.util.List;
+// import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.OneToMany;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "orders")
-public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+// @Entity
+// @Table(name = "orders")
+// public class Order {
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private long id;
 
-    private double totalPrice;
+// private double totalPrice;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+// @ManyToOne
+// @JoinColumn(name = "user_id")
+// private User user;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderDetail> orderDetails;
+// @OneToMany(mappedBy = "order")
+// private List<OrderDetail> orderDetails;
 
-    public long getId() {
-        return id;
-    }
+// public long getId() {
+// return id;
+// }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+// public void setId(long id) {
+// this.id = id;
+// }
 
-    public double getTotalPrice() {
-        return totalPrice;
-    }
+// public double getTotalPrice() {
+// return totalPrice;
+// }
 
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+// public void setTotalPrice(double totalPrice) {
+// this.totalPrice = totalPrice;
+// }
 
-    @Override
-    public String toString() {
-        return "Order [id=" + id + ", totalPrice=" + totalPrice + "]";
-    }
+// @Override
+// public String toString() {
+// return "Order [id=" + id + ", totalPrice=" + totalPrice + "]";
+// }
 
-}
+// }
--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -1,55 +1,56 @@
-package vn.hoidanit.laptopshop.domain;
+// package vn.hoidanit.laptopshop.domain;
 
-import java.util.List;
+// import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.OneToMany;
+// import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "roles")
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+// @Entity
+// @Table(name = "roles")
+// public class Role {
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private long id;
 
-    private String name;
+// private String name;
 
-    private String description;
+// private String description;
 
-    @OneToMany(mappedBy = "role")
-    private List<User> users;
+// @OneToMany(mappedBy = "role")
+// private List<User> users;
 
-    public long getId() {
-        return id;
-    }
+// public long getId() {
+// return id;
+// }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+// public void setId(long id) {
+// this.id = id;
+// }
 
-    public String getName() {
-        return name;
-    }
+// public String getName() {
+// return name;
+// }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+// public void setName(String name) {
+// this.name = name;
+// }
 
-    public String getDescription() {
-        return description;
-    }
+// public String getDescription() {
+// return description;
+// }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+// public void setDescription(String description) {
+// this.description = description;
+// }
 
-    @Override
-    public String toString() {
-        return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
-    }
+// @Override
+// public String toString() {
+// return "Role [id=" + id + ", name=" + name + ", description=" + description +
+// "]";
+// }
 
-}
+// }
