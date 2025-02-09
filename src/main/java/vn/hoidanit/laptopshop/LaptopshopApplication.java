a0c8c2feefbfdf3288126ca2029299c0a7d03fd5
John Doe <johndoe@email.com>
2025-02-09 15:35:40 +0700

    setup-security

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,12 +3,18 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
-
+@SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
+
+		// ApplicationContext hoidanit =
+		// SpringApplication.run(LaptopshopApplication.class, args);
+		// for (String s : hoidanit.getBeanDefinitionNames()) {
+		// System.out.println(s);
+		// }
+		// System.out.println("Running....");
 	}
 
 }
